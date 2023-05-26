45eac9e44dd0ba8923af87bc1e08b3a9beec918d
TrungK2Tan <trungtan25112k2@gmail.com>
2023-05-26 22:53:20 +0700

    Nop bai tuan 4

--- demo/src/main/java/com/example/demo/controller/BookController.java
+++ demo/src/main/java/com/example/demo/controller/BookController.java
@@ -3,13 +3,12 @@ package com.example.demo.controller;
 import com.example.demo.entity.Book;
 import com.example.demo.services.BookService;
 import com.example.demo.services.CategoryService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -33,9 +32,38 @@ public class BookController {
         return "book/add";
     }
     @PostMapping("/add")
-    public String addBook(@ModelAttribute("book") Book book){
+    public String addBook(@ModelAttribute("book") @Valid Book book, BindingResult bindingResult, Model model){
+        if (bindingResult.hasErrors()) {
+            // Xử lý lỗi ở đây
+            model.addAttribute("categories", categoryService.getAllCategories());
+            return "book/add";
+        }
         bookService.addBook(book);
         return "redirect:/books";
     }
+    @GetMapping("/edit/{id}")
+    public String editBooks(@PathVariable("id") Long id, Model model){
+        for(Book book : bookService.getAllBooks()){
+            if (book.getId().equals(id)){
+                model.addAttribute("book", book);
+                model.addAttribute("categories", categoryService.getAllCategories());
+                return "book/edit";
+            }
+        }
+        return "redirect:/books";
+    }
 
+    @PostMapping("/edit")
+    public String editBooks(@ModelAttribute("book") Book updateBook) {
+        // Logic để xử lý dữ liệu sách đã chỉnh sửa và lưu vào cơ sở dữ liệu
+        bookService.updateBook(updateBook);
+
+        // Chuyển hướng người dùng đến trang danh sách sách
+        return "redirect:/books";
+    }
+    @GetMapping("/delete/{id}")
+    public String deleteBooks(@PathVariable("id") Long id) {
+        bookService.deleteBook(id);
+        return "redirect:/books";
+    }
 }
--- demo/src/main/java/com/example/demo/services/CategoryService.java
+++ demo/src/main/java/com/example/demo/services/CategoryService.java
@@ -20,7 +20,15 @@ public class CategoryService {
     public Category saveCategory(Category category){
         return  categoryRepository.save(category);
     }
+    public Category addCategory(Category category) {
+        return categoryRepository.save(category);
+    }
+
+    public Category updateCategory(Category category) {
+        return categoryRepository.save(category);
+    }
     public void deleteCategory(Long id){
         categoryRepository.deleteById(id);
     }
 }
+
