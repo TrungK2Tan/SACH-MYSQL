e8e073575d76599dab737dad74b83b31f01f8a72
TrungK2Tan <trungtan25112k2@gmail.com>
2023-06-02 18:12:05 +0700

    Nop bai tuan 5

--- demo/src/main/java/com/example/demo/entity/CustomUserDetail.java
+++ demo/src/main/java/com/example/demo/entity/CustomUserDetail.java
@@ -2,10 +2,13 @@ package com.example.demo.entity;
 
 import com.example.demo.repository.IuserRepository;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.stream.Collectors;
 
 public class CustomUserDetail implements UserDetails {
     private  final  User user;
@@ -18,7 +21,9 @@ public class CustomUserDetail implements UserDetails {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return Collections.emptyList();
+        return Arrays.stream(userRepository.getRolesOfUser(user.getId()))
+                .map(SimpleGrantedAuthority::new)
+                .collect(Collectors.toSet());
     }
 
     @Override
--- demo/src/main/java/com/example/demo/services/UserService.java
+++ demo/src/main/java/com/example/demo/services/UserService.java
@@ -1,6 +1,9 @@
 package com.example.demo.services;
 
 import com.example.demo.entity.User;
+
+import com.example.demo.repository.IRoleRepository;
+
 import com.example.demo.repository.IuserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -9,7 +12,14 @@ import org.springframework.stereotype.Service;
 public class UserService {
     @Autowired
     private IuserRepository userRepository;
+    @Autowired
+    private IRoleRepository roleRepository;
     public void save(User user){
         userRepository.save(user);
+        Long userId = userRepository.getUserIdByUsername(user.getUsername());
+        Long roleId = roleRepository.getRoleIdByName("Member");
+        if(roleId!=0 && userId != 0){
+            userRepository.addRoleToUser(userId,roleId);
+        }
     }
 }
